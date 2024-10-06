21924b87676fd61f02fb6cfaf0f9ba30ead22440
Johannes Egger <j.egger@posteo.at>
2024-10-06 20:52:12 +0200

    Clean up tests

--- src/test/RegistrationController.fs
+++ src/test/RegistrationController.fs
@@ -5,26 +5,27 @@ open HTLVB.RegistrationForm.Server.Tests.Utils
 open Microsoft.AspNetCore.Http
 open Microsoft.AspNetCore.TestHost
 open System
+open System.Net.Http
 open System.Net.Http.Json
 open Xunit
 
-[<Fact>]
-let ``Can get released schedule`` () = async {
+let private getSampleSchedule reservationStartTimeOffset = async {
     use! server = InMemoryServer.start()
     use httpClient = server.GetTestClient()
     let reservationStartTime = FakeData.events.["tdot-2025"].ReservationStartTime
-    server |> InMemoryServer.setTimeProviderTime reservationStartTime
-    let! schedule = httpClient.GetFromJsonAsync<DataTransfer.Schedule>("/api/schedule/tdot-2025") |> Async.AwaitTask
+    server |> InMemoryServer.setTimeProviderTime (reservationStartTime + reservationStartTimeOffset)
+    return! httpClient.GetFromJsonAsync<DataTransfer.Schedule>("/api/schedule/tdot-2025") |> Async.AwaitTask
+}
+
+[<Fact>]
+let ``Can get released schedule`` () = async {
+    let! schedule = getSampleSchedule TimeSpan.Zero
     Assert.IsType<DataTransfer.ReleasedSchedule>(schedule) |> ignore
 }
 
 [<Fact>]
 let ``Can get hidden schedule`` () = async {
-    use! server = InMemoryServer.start()
-    use httpClient = server.GetTestClient()
-    let reservationStartTime = FakeData.events.["tdot-2025"].ReservationStartTime
-    server |> InMemoryServer.setTimeProviderTime (reservationStartTime - TimeSpan.FromSeconds 1.)
-    let! schedule = httpClient.GetFromJsonAsync<DataTransfer.Schedule>("/api/schedule/tdot-2025") |> Async.AwaitTask
+    let! schedule = getSampleSchedule (TimeSpan.FromSeconds -1.)
     Assert.IsType<DataTransfer.HiddenSchedule>(schedule) |> ignore
 }
 
@@ -36,57 +37,56 @@ let ``Doesn't find non-existing event`` () = async {
     Assert.Equal(enum StatusCodes.Status404NotFound, response.StatusCode)
 }
 
-[<Fact>]
-let ``Can make booking when enough capacity`` () = async {
-    use! server = InMemoryServer.start()
-    use httpClient = server.GetTestClient()
+let private getSampleReservationData quantity : DataTransfer.Subscriber =
+    {
+        Name = "Albert"
+        MailAddress = "albert@einstein.com"
+        PhoneNumber = "07612/123456789"
+        Quantity = quantity
+    }
+
+let private makeSampleBooking server (httpClient: HttpClient) pickBookingQuantity = async {
     let reservationStartTime = FakeData.events.["tdot-2025"].ReservationStartTime
     server |> InMemoryServer.setTimeProviderTime reservationStartTime
     let! schedule = httpClient.GetFromJsonAsync<DataTransfer.Schedule>("/api/schedule/tdot-2025") |> Async.AwaitTask
     let releasedSchedule = schedule :?> DataTransfer.ReleasedSchedule
-    let (slotUrl, slotCapacity) =
+    let (slotUrl, bookingQuantity) =
         releasedSchedule.Entries
         |> List.tryPick (fun v ->
             match v.ReservationType with
-            | :? DataTransfer.ReservationTypeFree as v when not v.MaxQuantityPerBooking.HasValue && v.RemainingCapacity.HasValue ->
-                Some (v.Url, v.RemainingCapacity.Value)
+            | :? DataTransfer.ReservationTypeFree as v ->
+                match pickBookingQuantity v with
+                | Some bookingCapacity -> Some (v.Url, bookingCapacity)
+                | None -> None
             | _ -> None
         )
-        |> Option.defaultWith (fun () -> Assert.Fail("No free slot found"); Unchecked.defaultof<_>)
-    let reservationData : DataTransfer.Subscriber = {
-        Name = "Albert"
-        MailAddress = "albert@einstein.com"
-        PhoneNumber = "07612/123456789"
-        Quantity = slotCapacity
-    }
-    let! response = httpClient.PostAsJsonAsync(slotUrl, reservationData) |> Async.AwaitTask
+        |> Option.defaultWith (fun () -> Assert.Fail("No slot found"); Unchecked.defaultof<_>)
+    return! httpClient.PostAsJsonAsync(slotUrl, getSampleReservationData bookingQuantity) |> Async.AwaitTask
+}
+
+let makeSampleBooking' pickBookingQuantity = async {
+    use! server = InMemoryServer.start()
+    use httpClient = server.GetTestClient()
+    return! makeSampleBooking server httpClient pickBookingQuantity
+}
+
+[<Fact>]
+let ``Can make booking when enough capacity`` () = async {
+    let! response = makeSampleBooking' (fun slot ->
+        if not slot.MaxQuantityPerBooking.HasValue && slot.RemainingCapacity.HasValue then
+            Some slot.RemainingCapacity.Value
+        else None
+    )
     Assert.Equal(enum StatusCodes.Status200OK, response.StatusCode)
 }
 
 [<Fact>]
 let ``Can't make booking when not enough capacity`` () = async {
-    use! server = InMemoryServer.start()
-    use httpClient = server.GetTestClient()
-    let reservationStartTime = FakeData.events.["tdot-2025"].ReservationStartTime
-    server |> InMemoryServer.setTimeProviderTime reservationStartTime
-    let! schedule = httpClient.GetFromJsonAsync<DataTransfer.Schedule>("/api/schedule/tdot-2025") |> Async.AwaitTask
-    let releasedSchedule = schedule :?> DataTransfer.ReleasedSchedule
-    let (slotUrl, slotCapacity) =
-        releasedSchedule.Entries
-        |> List.tryPick (fun v ->
-            match v.ReservationType with
-            | :? DataTransfer.ReservationTypeFree as v when not v.MaxQuantityPerBooking.HasValue && v.RemainingCapacity.HasValue ->
-                Some (v.Url, v.RemainingCapacity.Value)
-            | _ -> None
-        )
-        |> Option.defaultWith (fun () -> Assert.Fail("No free slot found"); Unchecked.defaultof<_>)
-    let reservationData : DataTransfer.Subscriber = {
-        Name = "Albert"
-        MailAddress = "albert@einstein.com"
-        PhoneNumber = "07612/123456789"
-        Quantity = slotCapacity + 1
-    }
-    let! response = httpClient.PostAsJsonAsync(slotUrl, reservationData) |> Async.AwaitTask
+    let! response = makeSampleBooking' (fun slot ->
+        if not slot.MaxQuantityPerBooking.HasValue && slot.RemainingCapacity.HasValue then
+            Some (slot.RemainingCapacity.Value + 1)
+        else None
+    )
     Assert.Equal(enum StatusCodes.Status400BadRequest, response.StatusCode)
 }
 
@@ -94,56 +94,24 @@ let ``Can't make booking when not enough capacity`` () = async {
 let ``Can make two consecutive bookings when enough capacity`` () = async {
     use! server = InMemoryServer.start()
     use httpClient = server.GetTestClient()
-    let reservationStartTime = FakeData.events.["tdot-2025"].ReservationStartTime
-    server |> InMemoryServer.setTimeProviderTime reservationStartTime
-    let! schedule = httpClient.GetFromJsonAsync<DataTransfer.Schedule>("/api/schedule/tdot-2025") |> Async.AwaitTask
-    let releasedSchedule = schedule :?> DataTransfer.ReleasedSchedule
-    let slotUrl =
-        releasedSchedule.Entries
-        |> List.tryPick (fun v ->
-            match v.ReservationType with
-            | :? DataTransfer.ReservationTypeFree as v when v.RemainingCapacity.HasValue && v.RemainingCapacity.Value > 1 ->
-                Some v.Url
-            | _ -> None
-        )
-        |> Option.defaultWith (fun () -> Assert.Fail("No free slot found"); Unchecked.defaultof<_>)
-    let reservationData : DataTransfer.Subscriber = {
-        Name = "Albert"
-        MailAddress = "albert@einstein.com"
-        PhoneNumber = "07612/123456789"
-        Quantity = 1
-    }
-    let! response = httpClient.PostAsJsonAsync(slotUrl, reservationData) |> Async.AwaitTask
+    let! response = makeSampleBooking server httpClient (fun slot ->
+        if slot.RemainingCapacity.HasValue && slot.RemainingCapacity.Value > 1 then
+            Some 1
+        else None
+    )
     let! bookingResult = response.Content.ReadFromJsonAsync<DataTransfer.BookingResult>() |> Async.AwaitTask
     let reservationTypeFree = bookingResult.ReservationType :?> DataTransfer.ReservationTypeFree
-    let! response = httpClient.PostAsJsonAsync(reservationTypeFree.Url, reservationData) |> Async.AwaitTask
+    let! response = httpClient.PostAsJsonAsync(reservationTypeFree.Url, getSampleReservationData 1) |> Async.AwaitTask
     Assert.Equal(enum StatusCodes.Status200OK, response.StatusCode)
 }
 
 [<Fact>]
 let ``Can make booking when no capacity limit`` () = async {
-    use! server = InMemoryServer.start()
-    use httpClient = server.GetTestClient()
-    let reservationStartTime = FakeData.events.["tdot-2025"].ReservationStartTime
-    server |> InMemoryServer.setTimeProviderTime reservationStartTime
-    let! schedule = httpClient.GetFromJsonAsync<DataTransfer.Schedule>("/api/schedule/tdot-2025") |> Async.AwaitTask
-    let releasedSchedule = schedule :?> DataTransfer.ReleasedSchedule
-    let slotUrl =
-        releasedSchedule.Entries
-        |> List.tryPick (fun v ->
-            match v.ReservationType with
-            | :? DataTransfer.ReservationTypeFree as v when not v.MaxQuantityPerBooking.HasValue && not v.RemainingCapacity.HasValue ->
-                Some v.Url
-            | _ -> None
-        )
-        |> Option.defaultWith (fun () -> Assert.Fail("No free slot found"); Unchecked.defaultof<_>)
-    let reservationData : DataTransfer.Subscriber = {
-        Name = "Albert"
-        MailAddress = "albert@einstein.com"
-        PhoneNumber = "07612/123456789"
-        Quantity = Int32.MaxValue
-    }
-    let! response = httpClient.PostAsJsonAsync(slotUrl, reservationData) |> Async.AwaitTask
+    let! response = makeSampleBooking' (fun slot ->
+        if not slot.MaxQuantityPerBooking.HasValue && not slot.RemainingCapacity.HasValue then
+            Some Int32.MaxValue
+        else None
+    )
     let! bookingResult = response.Content.ReadFromJsonAsync<DataTransfer.BookingResult>() |> Async.AwaitTask
     let reservationTypeFree = bookingResult.ReservationType :?> DataTransfer.ReservationTypeFree
     Assert.Equal(None, Option.ofNullable reservationTypeFree.RemainingCapacity)
@@ -151,28 +119,11 @@ let ``Can make booking when no capacity limit`` () = async {
 
 [<Fact>]
 let ``Can't make booking when quantity > max quantity per booking`` () = async {
-    use! server = InMemoryServer.start()
-    use httpClient = server.GetTestClient()
-    let reservationStartTime = FakeData.events.["tdot-2025"].ReservationStartTime
-    server |> InMemoryServer.setTimeProviderTime reservationStartTime
-    let! schedule = httpClient.GetFromJsonAsync<DataTransfer.Schedule>("/api/schedule/tdot-2025") |> Async.AwaitTask
-    let releasedSchedule = schedule :?> DataTransfer.ReleasedSchedule
-    let (slotUrl, maxQuantityPerBooking) =
-        releasedSchedule.Entries
-        |> List.tryPick (fun v ->
-            match v.ReservationType with
-            | :? DataTransfer.ReservationTypeFree as v when v.MaxQuantityPerBooking.HasValue && (not v.RemainingCapacity.HasValue || v.RemainingCapacity.Value > v.MaxQuantityPerBooking.Value) ->
-                Some (v.Url, v.MaxQuantityPerBooking.Value)
-            | _ -> None
-        )
-        |> Option.defaultWith (fun () -> Assert.Fail("No free slot found"); Unchecked.defaultof<_>)
-    let reservationData : DataTransfer.Subscriber = {
-        Name = "Albert"
-        MailAddress = "albert@einstein.com"
-        PhoneNumber = "07612/123456789"
-        Quantity = maxQuantityPerBooking + 1
-    }
-    let! response = httpClient.PostAsJsonAsync(slotUrl, reservationData) |> Async.AwaitTask
+    let! response = makeSampleBooking' (fun slot ->
+        if slot.MaxQuantityPerBooking.HasValue && (not slot.RemainingCapacity.HasValue || slot.RemainingCapacity.Value > slot.MaxQuantityPerBooking.Value) then
+            Some (slot.MaxQuantityPerBooking.Value + 1)
+        else None
+    )
     Assert.Equal(enum StatusCodes.Status400BadRequest, response.StatusCode)
 }
 
