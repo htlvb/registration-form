36ea29d247d26366ddf909318f8e6fc605fe1586
Johannes Egger <j.egger@posteo.at>
2024-10-31 22:00:52 +0100

    Format code

--- src/client/src/components/SlotSelection.vue
+++ src/client/src/components/SlotSelection.vue
@@ -88,58 +88,64 @@ const pluralize = (v: number, singularText: string, pluralText: string) => {
 </script>
 
 <template>
-    <div v-if="slotSummaries.length > 1">
-        <h2 class="text-lg">Datum</h2>
-        <div class="mt-2 flex flex-wrap gap-2">
-        <template v-for="slotSummary in slotSummaries" :key="slotSummary.date">
-            <button v-if="slotSummary.hasUnlimitedCapacity" type="button" class="!flex flex-col items-center justify-center button"
-            :class="{ 'button-htlvb-selected': selectedDate === slotSummary.date }" @click="selectedDate = slotSummary.date">
-            <span>{{ DateTime.formatDate(new Date(slotSummary.date), { weekday: 'short' }) }}</span>
-            </button>
-            <button v-else-if="slotSummary.freeSlots > 0" type="button" class="!flex flex-col items-center button"
-            :class="{ 'button-htlvb-selected': selectedDate === slotSummary.date }" @click="selectedDate = slotSummary.date">
-            <span>{{ DateTime.formatDate(new Date(slotSummary.date), { weekday: 'short' }) }}</span>
-            <span class="text-sm">{{ pluralize(slotSummary.freeSlots, 'freier Platz', 'freie Plätze') }}</span>
-            </button>
-            <button v-else-if="slotSummary.hasClosedSlot" type="button" :disabled="true" class="!flex flex-col items-center button">
-            <span>{{ DateTime.formatDate(new Date(slotSummary.date), { weekday: 'short' }) }}</span>
-            <span class="text-sm">geschlossen</span>
-            </button>
-            <button v-else-if="slotSummary.hasTakenSlot" type="button" :disabled="true" class="!flex flex-col items-center button">
-            <span>{{ DateTime.formatDate(new Date(slotSummary.date), { weekday: 'short' }) }}</span>
-            <span class="text-sm">ausgebucht</span>
-            </button>
-        </template>
-        </div>
+  <div v-if="slotSummaries.length > 1">
+    <h2 class="text-lg">Datum</h2>
+    <div class="mt-2 flex flex-wrap gap-2">
+      <template v-for="slotSummary in slotSummaries" :key="slotSummary.date">
+        <button v-if="slotSummary.hasUnlimitedCapacity" type="button"
+          class="!flex flex-col items-center justify-center button"
+          :class="{ 'button-htlvb-selected': selectedDate === slotSummary.date }"
+          @click="selectedDate = slotSummary.date">
+          <span>{{ DateTime.formatDate(new Date(slotSummary.date), { weekday: 'short' }) }}</span>
+        </button>
+        <button v-else-if="slotSummary.freeSlots > 0" type="button" class="!flex flex-col items-center button"
+          :class="{ 'button-htlvb-selected': selectedDate === slotSummary.date }"
+          @click="selectedDate = slotSummary.date">
+          <span>{{ DateTime.formatDate(new Date(slotSummary.date), { weekday: 'short' }) }}</span>
+          <span class="text-sm">{{ pluralize(slotSummary.freeSlots, 'freier Platz', 'freie Plätze') }}</span>
+        </button>
+        <button v-else-if="slotSummary.hasClosedSlot" type="button" :disabled="true"
+          class="!flex flex-col items-center button">
+          <span>{{ DateTime.formatDate(new Date(slotSummary.date), { weekday: 'short' }) }}</span>
+          <span class="text-sm">geschlossen</span>
+        </button>
+        <button v-else-if="slotSummary.hasTakenSlot" type="button" :disabled="true"
+          class="!flex flex-col items-center button">
+          <span>{{ DateTime.formatDate(new Date(slotSummary.date), { weekday: 'short' }) }}</span>
+          <span class="text-sm">ausgebucht</span>
+        </button>
+      </template>
     </div>
-    <template v-if="selectedDate !== undefined">
-        <h2 class="text-lg mt-4">Uhrzeit</h2>
-        <div class="mt-2 flex flex-wrap gap-2">
-        <template v-for="slot in slots.filter(v => selectedDate !== undefined && DateTime.dateEquals(new Date(selectedDate), new Date(v.startTime)))"
-            :key="slot.startTime">
-            <template v-if="slot.type.type === 'free'">
-            <button v-if="slot.type.remainingCapacity === null" type="button" class="!flex flex-col items-center justify-center button"
-                :class="{ 'button-htlvb-selected': selectedSlot === slot }" @click="selectedSlot = slot">
-                <span>{{ formatSlotTime(slot) }}</span>
-            </button>
-            <button v-else type="button" class="!flex flex-col items-center button"
-                :class="{ 'button-htlvb-selected': selectedSlot === slot }" @click="selectedSlot = slot">
-                <span>{{ formatSlotTime(slot) }}</span>
-                <span class="text-sm">{{ pluralize(slot.type.remainingCapacity, 'freier Platz', 'freie Plätze') }}</span>
-            </button>
-            </template>
-            <button v-else-if="slot.type.type === 'closed'" type="button" :disabled="true" class="!flex flex-col items-center button">
+  </div>
+  <template v-if="selectedDate !== undefined">
+    <h2 class="text-lg mt-4">Uhrzeit</h2>
+    <div class="mt-2 flex flex-wrap gap-2">
+      <template v-for="slot in slots.filter(v => selectedDate !== undefined && DateTime.dateEquals(new Date(selectedDate), new Date(v.startTime)))"
+        :key="slot.startTime">
+        <template v-if="slot.type.type === 'free'">
+          <button v-if="slot.type.remainingCapacity === null" type="button"
+            class="!flex flex-col items-center justify-center button"
+            :class="{ 'button-htlvb-selected': selectedSlot === slot }" @click="selectedSlot = slot">
             <span>{{ formatSlotTime(slot) }}</span>
-            <span class="text-sm">geschlossen</span>
-            </button>
-            <button v-else-if="slot.type.type === 'taken'" type="button" :disabled="true" class="!flex flex-col items-center button">
+          </button>
+          <button v-else type="button" class="!flex flex-col items-center button"
+            :class="{ 'button-htlvb-selected': selectedSlot === slot }" @click="selectedSlot = slot">
             <span>{{ formatSlotTime(slot) }}</span>
-            <span class="text-sm">ausgebucht</span>
-            </button>
+            <span class="text-sm">{{ pluralize(slot.type.remainingCapacity, 'freier Platz', 'freie Plätze') }}</span>
+          </button>
         </template>
-        </div>
-        <div v-if="selectedSlot !== undefined && selectedSlot.type.type === 'free' && selectedSlot.type.closingDate !== null" class="mt-2">
-        <span class="text-sm">Anmeldeschluss: {{ formatClosingDate(new Date(selectedSlot.type.closingDate)) }}</span>
-        </div>
-    </template>
+        <button v-else-if="slot.type.type === 'closed'" type="button" :disabled="true" class="!flex flex-col items-center button">
+          <span>{{ formatSlotTime(slot) }}</span>
+          <span class="text-sm">geschlossen</span>
+        </button>
+        <button v-else-if="slot.type.type === 'taken'" type="button" :disabled="true" class="!flex flex-col items-center button">
+          <span>{{ formatSlotTime(slot) }}</span>
+          <span class="text-sm">ausgebucht</span>
+        </button>
+      </template>
+    </div>
+    <div v-if="selectedSlot !== undefined && selectedSlot.type.type === 'free' && selectedSlot.type.closingDate !== null" class="mt-2">
+      <span class="text-sm">Anmeldeschluss: {{ formatClosingDate(new Date(selectedSlot.type.closingDate)) }}</span>
+    </div>
+  </template>
 </template>
